83519d28999a72d830fd0ae84f8c7efcd01d01ee
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2021-08-09 13:21:22 +0800

    Update randomPicker.py

--- randomPicker.py
+++ randomPicker.py
@@ -2,6 +2,7 @@ import random
 
 movies_list = ['The Godfather', 'The Wizard of Oz', 'Citizen Kane', 'The Shawshank Redemption', 'Pulp Fiction']
 
+
 # pick a random choice from a list of strings.
 movie = random.choice(movies_list)
 print(movie
